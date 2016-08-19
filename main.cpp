c775b00e2c13d67be7bafc1f68f257fca25571b9
suzusime <tamagawajousuiro@takechiyo.net>
2016-08-19 15:26:43 +0900

    正規表現の部分を修正

--- main.cpp
+++ main.cpp
@@ -20,44 +20,44 @@ std::string get_text(std::ifstream& ifs_){
 
 std::string remove_comment(std::string src){
 	using namespace std;
-	regex re("≪.*?≫");
+	regex re( R"(≪.*?≫)" );
 	string dest = regex_replace(src, re, "");
 	return dest;
 }
 
 std::string style_h1(std::string src){
 	using namespace std;
-	regex re("■(.*)");//行頭という指定をなくすと何故かうまくいく
+	regex re( R"(■(.*))" );//文字列の先頭にしか^がマッチしないので行頭にマッチさせるのどうすればいいんだろうなぁ……
 	string dest = regex_replace(src, re, "<h1>$1</h1>");
 	return dest;
 }
 std::string style_h2(std::string src){
 	using namespace std;
-	regex re("◆(.*)");
+	regex re( R"(◆(.*))" );
 	string dest = regex_replace(src, re, "<h2>$1</h2>");
 	return dest;
 }
 std::string style_h3(std::string src){
 	using namespace std;
-	regex re("◎(.*)");
+	regex re( R"(◎(.*))" );
 	string dest = regex_replace(src, re, "<h3>$1</h3>");
 	return dest;
 }
 std::string style_h4(std::string src){
 	using namespace std;
-	regex re("○(.*)");
+	regex re( R"(○(.*))" );
 	string dest = regex_replace(src, re, "<h4>$1</h4>");
 	return dest;
 }
 std::string style_h5(std::string src){
 	using namespace std;
-	regex re("△(.*)");
+	regex re( R"(△(.*))" );
 	string dest = regex_replace(src, re, "<h5>$1</h5>");
 	return dest;
 }
 std::string style_h6(std::string src){
 	using namespace std;
-	regex re("☆(.*)");
+	regex re( R"(☆(.*))" );
 	string dest = regex_replace(src, re, "<h6>$1</h6>");
 	return dest;
 }
@@ -83,7 +83,7 @@ std::string embed_to_html(std::string src){
 		return "";
 	}
 	string str = get_text(ifs);
-	regex re("＠本文");
+	regex re( R"(＠本文)" );
 	string dest = regex_replace(str, re, src);
 	return dest;
 }
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all:
-	g++ -std=c++11 -o yomika main.cpp
+	g++ -std=c++14 -o yomika main.cpp
